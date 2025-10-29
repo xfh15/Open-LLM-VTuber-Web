d3114131b1680df400996db256542ac49c80aaef
sirius <sirius@siriusdeMacBook-Pro.local>
2025-10-29 09:22:55 +0900

    修改默认配置

--- src/renderer/src/context/websocket-context.tsx
+++ src/renderer/src/context/websocket-context.tsx
@@ -3,8 +3,8 @@ import React, { useContext, useCallback } from 'react';
 import { wsService } from '@/services/websocket-service';
 import { useLocalStorage } from '@/hooks/utils/use-local-storage';
 
-const DEFAULT_WS_URL = 'ws://127.0.0.1:12393/client-ws';
-const DEFAULT_BASE_URL = 'http://127.0.0.1:12393';
+const DEFAULT_WS_URL = 'wss://digital.z-soft.jp:12394/client-ws';
+const DEFAULT_BASE_URL = 'https://digital.z-soft.jp:12394';
 
 export interface HistoryInfo {
   uid: string;
